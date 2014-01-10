09857fee9037c36f0e9c3e9f7504b0608b46367b
alb423 <alb423@gmail.com>
2014-01-10 17:52:35 +0800

    Try to receive remote event and control

--- FFmpegAudioPlayer/AppDelegate.m
+++ FFmpegAudioPlayer/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+#import "MediaPlayer/MPNowPlayingInfoCenter.h"
+#import "MediaPlayer/MPMediaItem.h"
 
 @implementation AppDelegate
 
@@ -26,11 +28,14 @@
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    [application beginReceivingRemoteControlEvents];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    [application endReceivingRemoteControlEvents];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
@@ -41,6 +46,71 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+
+#pragma mark - control when screen is locked
+
+- (void)remoteControlReceivedWithEvent:(UIEvent *)event {
+    
+    if(event.type== UIEventTypeRemoteControl)  {
+        
+        NSLog(@"Remote Control Type: %d", event.subtype);
+        
+        switch (event.subtype) {
+                
+            case UIEventSubtypeRemoteControlTogglePlayPause:
+                
+                NSLog(@"UIEventSubtypeRemoteControlTogglePlayPause");
+                
+                break;
+                
+                
+            case UIEventSubtypeRemoteControlNextTrack:
+                
+                NSLog(@"UIEventSubtypeRemoteControlNextTrack");
+                
+                break;
+                
+                
+            case UIEventSubtypeRemoteControlPreviousTrack:
+                
+                NSLog(@"UIEventSubtypeRemoteControlPreviousTrack");
+                
+                break;
+                
+            case UIEventSubtypeRemoteControlPause:
+                NSLog(@"UIEventSubtypeRemoteControlPause");
+                break;
+            case UIEventSubtypeRemoteControlPlay:
+                NSLog(@"UIEventSubtypeRemoteControlPlay");
+                break;
+                
+            default:
+                break;
+        }
+    }
+}
+
+- (void)configNowPlayingInfoCenter {
+    
+    Class playingInfoCenter = NSClassFromString(@"MPNowPlayingInfoCenter");
+    if (playingInfoCenter) {
+        
+        MPNowPlayingInfoCenter *center = [MPNowPlayingInfoCenter defaultCenter];
+        
+        // 当前播放歌曲的图片
+        //MPMediaItemArtwork *artwork = [[MPMediaItemArtwork alloc] initWithImage: Default];
+        
+        NSDictionary *songInfo = [NSDictionary dictionaryWithObjectsAndKeys:@"艺术家名", MPMediaItemPropertyArtist,
+                                  @"歌曲名", MPMediaItemPropertyTitle,
+                                  nil/*artwork*/, MPMediaItemPropertyArtwork,
+                                  @"专辑名", MPMediaItemPropertyAlbumTitle,
+                                  nil];
+        center.nowPlayingInfo = songInfo;
+        
+        //[artwork release];
+    }
 }
 
 @end
